555aaa4f79f6b0c1f903fa8ac6369a78002c7c9c
Jeffrey Rose <jtr219@outlook.com>
2021-04-05 00:05:41 -0400

    fixed error on types

--- src/strategies/pairFinder.ts
+++ src/strategies/pairFinder.ts
@@ -1,16 +1,16 @@
 // Find all groupings of unique combinations by group length, ignoring order
 // Ex: unique pairs of [1,2,3,4] => [1,2], [1,3], [1,4], [2,3], [2,4], [3,4]
+// this works on either any[] or nested any[][]
+// Ex: pairs of [[1,2], [3,4], [5,6]] => [[[1,2], [3,4]], [[1,2], [5,6]], [[3,4], [5,6]]]
 
-type FindGroups = (
-  update: number[][],
-  counter: number
-) => FindGroups | number[][]
+type FindGroups = (update: any[][], counter: number) => FindGroups | any[][]
 
-export const findGroups = (groupLength: number) => (base: number[]) => {
+export const findGroups = (groupLength: number) => (base: any[]) => {
+  // convert array of any[] to any[][]
   const initialPairings = base.flatMap((x, i) =>
     base.slice(i + 1).map((y) => [x, y])
   )
-  const fn: FindGroups = (update: number[][], counter: number) => {
+  const fn: FindGroups = (update, counter) => {
     if (counter === groupLength) {
       return update
     }
@@ -19,7 +19,7 @@ export const findGroups = (groupLength: number) => (base: number[]) => {
     )
     return fn(updateArray, counter + 1)
   }
-  return fn(initialPairings, 1)
+  return fn(initialPairings, 2)
 }
 
 export const pairs = findGroups(2)
